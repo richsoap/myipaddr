b125e73531ac97e13d356433359e60067340fb6d
richsoap <1500012956@pku.edu.cn>
2017-11-21 22:50:19 +0800

    2017年 11月 21日 星期二 22:50:19 CST

--- autoconnect.sh
+++ autoconnect.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
 
+getNowIP() {
+	tempString=$(./connect -u 1500012956 -p cppshidi1)
+	tempString=${tempString#*IP}
+	tempString=${tempString%C*}
+	length=`expr ${#tempString} - 2`
+	if [ $length == 50 ]
+	then
+		tempString="FULL"
+	elif [ $length == 111 ]
+	then
+		tempString="ERROR"
+	else
+		tempString=${tempString:2:$length}
+	fi
+	return $tempString
+}
+
+syncGit() {
+	pushdate=$(date)
+	git add .
+	git commit -m "${pushdate}"
+	git push origin master
+}
+
+
 cd ~/.mycmd/myipaddr/
 
 while true
 do
-read oldIP < ./ipaddr.txt
-nowIP=getNowIP()
+read oldIP < ipaddr.txt
+
+getNowIP
+nowIP=$?
+
 case $nowIP in
 	"FULL")
-		echo "close all"
-		nowIP=getNowIP()
+		./connect -u 1500012956 -p cppshidi1 -c
+		getNowIP
+		nowIP=$?
 		;;
 	"ERROR")
 		continue
@@ -19,30 +48,11 @@ esac
 if [ $oldIP != $nowIP ]
 then
 	echo $nowIP > ipaddr.txt
-	syncGit()
+	syncGit
 fi
 sleep 5m
 done
 
-syncGit() {
-	pushdate=$(date)
-	git commit -m "${pushdate}"
-	git push origin master
-}
 
-getNowIP() {
-	tempString=$(./connect -u 1500012956 -p cppshidi1)
-	tempString=${tempString#*IP}
-	tempString=${tempString%C*}
-	length=`expr ${#tempString} - 2`
-	if [ $length == 20 ]
-	then
-		tempString="FULL"
-	elif [ $length == 25 ]
-	then
-		tempString="ERROR"
-	else
-		tempString=${tempString:2:$length}
-	fi
-	return $tempString
-}
+
+
