9eb2953530c8bd2f90c94562df2a927df83aaf69
richsoap <1500012956@pku.edu.cn>
2017-11-21 20:47:14 +0800

    test version

--- autoconnect.sh
+++ autoconnect.sh
@@ -1,11 +1,48 @@
 #!/bin/bash
 
 cd ~/.mycmd/myipaddr/
-# test
 
+while true
+do
+read oldIP < ./ipaddr.txt
+nowIP=getNowIP()
+case $nowIP in
+	"FULL")
+		echo "close all"
+		nowIP=getNowIP()
+		;;
+	"ERROR")
+		continue
+		;;
+esac
+		
+if [ $oldIP != $nowIP ]
+then
+	echo $nowIP > ipaddr.txt
+	syncGit()
+fi
+sleep 5m
+done
 
-ip addr show > ipaddr.txt
-git add .
-pushdate=$(date)
-git commit -m "${pushdate}"
-git push origin master
+syncGit() {
+	pushdate=$(date)
+	git commit -m "${pushdate}"
+	git push origin master
+}
+
+getNowIP() {
+	tempString=$(./connect -u 1500012956 -p cppshidi1)
+	tempString=${tempString#*IP}
+	tempString=${tempString%C*}
+	length=`expr ${#tempString} - 2`
+	if [ $length == 20 ]
+	then
+		tempString="FULL"
+	elif [ $length == 25 ]
+	then
+		tempString="ERROR"
+	else
+		tempString=${tempString:2:$length}
+	fi
+	return $tempString
+}
